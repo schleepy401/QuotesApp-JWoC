c05e448c2705e3387627a9966dd267bd9a65ef91
ahamed <ahamedibrahim2002@gmail.com>
2022-02-04 21:34:37 +0530

    Added Recycler View

--- app/src/main/java/com/jwoc/quotesapp/MainActivity.kt
+++ app/src/main/java/com/jwoc/quotesapp/MainActivity.kt
@@ -2,10 +2,30 @@ package com.jwoc.quotesapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.viewpager2.widget.ViewPager2
+import com.jwoc.quotesapp.RecylerView.RecyclerViewAdapter
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //inialization
+        val viewPager : ViewPager2 =findViewById(R.id.viewpager);
+
+        //data's
+        //info's ->  I have created two Static Array ,one for saving quotes ("message") and another is saving authors("authors")
+        val message : Array<String> =arrayOf("Don't let Your happiness depend on something you may lose",
+            "Fantacy is hardly An Escape From Reality It's A Way Of understanding It",
+            "When Something is Important enough,you Do It Even If The Odds Aren't In Yout Favor",
+            "In Life Don't react always respond",
+            "It's Only Bad Day Not Bad Life",
+            "All arts is kind of confussion");
+        val authors : Array<String> =arrayOf("C.S.lewis","Loyd Alexander",
+            "Elon Musk",
+            "Sundar Pitchai",
+            "Johnny Depp",
+            "James Baldwin")
+        viewPager.adapter = RecyclerViewAdapter(message,authors)
     }
 }
